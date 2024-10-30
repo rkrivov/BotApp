f75d2366da11fb50b8178b2d957ff27cd5c27eab
Roman "Bres" Krivov <rn.krivov@gmail.com>
2024-10-30 21:50:47 +0300

    The TimeAndDate project has been removed from the solution. The implementation has been postponed for the future.

--- BotApp.Service/Api/Astronomy/Moon/MoonHelper.cs
+++ BotApp.Service/Api/Astronomy/Moon/MoonHelper.cs
@@ -2,32 +2,40 @@
 using Telegram.Bot;
 using Telegram.Bot.Types;
 
-using TimeAndDate.Services;
-using TimeAndDate.Services.DataTypes.Astro;
-using TimeAndDate.Services.DataTypes.Places;
+ //using TimeAndDate.Services;
+ //using TimeAndDate.Services.DataTypes.Astro;
+ //using TimeAndDate.Services.DataTypes.Places;
 
 
 namespace BotApp.Service.Api.Astronomy.Moon
 {
     internal static class MoonHelper
     {
-        public static async Task GetMoonPhase(decimal latitude, decimal longtitude, DateTime dateTime)
+        public static Task GetMoonPhase(decimal latitude, decimal longtitude, DateTime dateTime)
         {
+            /*
             var service = new AstronomyService(AuthorizeTokensConsts.TDAccessKey, AuthorizeTokensConsts.TDSecretKey);
 
             var coordinates = new Coordinates(latitude, longtitude);
 
             var placeId = new LocationId(coordinates);
             var result = await service.GetAstronomicalInfoAsync(AstronomyObjectType.Moon, placeId, dateTime);
+            */
+
+            return Task.CompletedTask;
         }
-        public static async Task GetMoonPhase(decimal latitude, decimal longtitude, DateTime startDateTime, DateTime endDateTime)
+        public static Task GetMoonPhase(decimal latitude, decimal longtitude, DateTime startDateTime, DateTime endDateTime)
         {
+            /*
             var service = new AstronomyService(AuthorizeTokensConsts.TDAccessKey, AuthorizeTokensConsts.TDSecretKey);
 
             var coordinates = new Coordinates(latitude, longtitude);
 
             var placeId = new LocationId(coordinates);
             var result = await service.GetAstronomicalInfoAsync(AstronomyObjectType.Moon, placeId, startDateTime, endDateTime);
+            */
+
+            return Task.CompletedTask;
         }
     }
 }
